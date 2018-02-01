a7172a8d87258ffd5b9ffccf871630a0cdc701d7
Brad McGonigle <brad@bradmcgonigle.com>
2018-02-01 17:01:40 -0500

    Update About page

--- src/pages/about/about.md
+++ src/pages/about/about.md
@@ -1,10 +1,16 @@
 ---
 templateKey: 'about-page'
 path: /about
-title: About Me
+title: About
+image: me.jpg
 ---
-I'm Brad McGonigle, a web developer and _sudo_-designer from Orlando, Florida.
 
-When I'm not creating things on the web, I spend my time with my gorgeous wife Shaina. We have an awesome kitty, Mila, who shockingly likes to mostly sleep.  I'm an avid surfer, hockey player and all-round sports fan. _Fly Eagles Fly!_
+Hi, I'm Brad — a frontend developer and product designer from Orlando, Florida.
+
+I've been working both as a product designer and frontend developer for over 10 years. I've been lucky enough to work with and for a wide variety of companies ranging from large corporate environments to agency client driven shops to venture capital funded startups.
+
+When I'm not creating things on the web, I spend my time with my wife Shaina and our kitty Mila.
+
+I'm an avid surfer, hockey player and all-round sports fan. _Fly Eagles Fly!_
 
 I like to pretend to be a chef with recent adventures into Sous Vide and smoking everything from meats to salt.
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import styled from 'react-emotion';
 import graphql from 'graphql';
+
+import Img from 'gatsby-image';
 
 import Content, { HTMLContent } from '../components/content';
 
-
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = (
+  { title, image, content, contentComponent }
+) => {
   const PageContent = contentComponent || Content;
 
   return (
     <section className="section">
       <div className="container content">
         <div className="columns">
-          <div className="column is-6">
+          <div className="column is-5">
+            <Img className="image" sizes={image.childImageSharp.sizes} />
+          </div>
+          <div className="column is-6 is-offset-1">
             <h1 className="title">{title}</h1>
             <PageContent className="content" content={content} />
           </div>
@@ -23,10 +30,12 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
 
 export default ({ data }) => {
   const { markdownRemark: post } = data;
+  console.log(data);
 
   return (<AboutPageTemplate
     contentComponent={HTMLContent}
     title={post.frontmatter.title}
+    image={post.frontmatter.image}
     content={post.html}
   />);
 };
@@ -38,6 +47,17 @@ export const aboutPageQuery = graphql`
       frontmatter {
         path
         title
+        image {
+          childImageSharp {
+            sizes(
+              maxWidth: 1000,
+              cropFocus: ENTROPY,
+              quality: 90
+            ) {
+              ...GatsbyImageSharpSizes_withWebp
+            }
+          }
+        }
       }
     }
   }
