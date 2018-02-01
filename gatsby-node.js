b66434f6792cef5d341960b5a363d9b6c97586e5
Brad McGonigle <brad@bradmcgonigle.com>
2018-02-01 12:22:14 -0500

    Update gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
@@ -6,7 +6,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
   return graphql(`
     {
       allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
+        sort: { order: DESC, fields: [frontmatter___date] },
         limit: 1000
       ) {
         edges {
@@ -17,30 +17,25 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
             frontmatter {
               templateKey
               path
-              date
               title
+              date
             }
           }
         }
       }
     }
-  `).then(result => {
+  `).then((result) => {
     if (result.errors) {
       result.errors.forEach(e => console.error(e.toString()));
       return Promise.reject(result.errors);
     }
 
     return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      const pagePath = node.frontmatter.path;
+      console.log(node);
       createPage({
-        path: pagePath,
-        component: path.resolve(
-          `src/templates/${String(node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          path: pagePath
-        }
+        path: node.frontmatter.path,
+        component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
+        context: {}, // additional data can be passed via context
       });
     });
   });
