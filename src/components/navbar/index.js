e89a78ff77ef33dbf3ea1dc9aa98b2c3e909130a
Brad McGonigle <brad@bradmcgonigle.com>
2018-02-02 16:09:29 -0500

    Simplify navigation styles - Remove blog - Remove hover state underlines

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,43 +7,6 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faFacebook, faGithub, faInstagram, faTwitter } from '@fortawesome/fontawesome-free-brands';
 
 
-const Brand = styled('div')`
-  a.navbar-item:hover {
-    background-color: inherit!important;
-  }
-`;
-
-const NavbarMenu = styled('div')`
-  .navbar-item.site-nav {
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    margin-left: 0.5rem;
-    margin-right: 0.5rem;
-    position: relative;
-    transition: color .5s cubic-bezier(.19, 1, .22, 1);
-
-    &:after {
-      background-color: rgba(0, 0, 0, 0.025);
-      bottom: 10%;
-      content: "";
-      height: 6px;
-      left: 0;
-      position: absolute;
-      transform-origin: center top;
-      transform: scaleX(0);
-      transition: transform .5s cubic-bezier(.19, 1, .22, 1);
-      width: 100%;
-    }
-
-    &:hover {
-      &:after {
-        transform: scaleX(1);
-        transform-origin: center bottom
-      }
-    }
-  }
-`;
-
 const SocialLink = styled('a')`
   padding-left: 0.33rem;
   padding-right: 0.33rem;
@@ -52,7 +15,7 @@ const SocialLink = styled('a')`
 const Navbar = () => (
   <nav className="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
     <div className="container">
-      <Brand className="navbar-brand">
+      <div className="navbar-brand">
         <Link to="/" className="navbar-item is-info">
           <strong>B &mdash; M.</strong>
         </Link>
@@ -61,14 +24,11 @@ const Navbar = () => (
           <span></span>
           <span></span>
         </div>
-      </Brand>
-      <NavbarMenu id="navbar" className="navbar-menu">
+      </div>
+      <div id="navbar" className="navbar-menu">
         <div className="navbar-end">
           <Link className="navbar-item site-nav" to="/about">
             About
-          </Link>
-          <Link className="navbar-item site-nav" to="/blog">
-            Blog
           </Link>
           <Link className="navbar-item site-nav" to="/links">
             Links
@@ -87,7 +47,7 @@ const Navbar = () => (
             <span className="icon"><FontAwesomeIcon icon={faFacebook} inverse /></span>
           </SocialLink>
         </div>
-      </NavbarMenu>
+      </div>
     </div>
   </nav>
 );
