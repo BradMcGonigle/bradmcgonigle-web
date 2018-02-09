6b7ab9a8944d1ffce8e7a1d0c30c2d1453cbb2a8
Brad McGonigle <brad@bradmcgonigle.com>
2018-02-09 11:00:00 -0500

    Make navbar transparent with background blur for supporting browsers.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,13 +7,18 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faFacebook, faGithub, faInstagram, faTwitter } from '@fortawesome/fontawesome-free-brands';
 
 
+const NavbarWrapper = styled('nav')`
+  backdrop-filter: saturate(200%) blur(25px);
+  background-color: rgba(255, 255, 255, 0.75);
+`;
+
 const SocialLink = styled('a')`
   padding-left: 0.33rem;
   padding-right: 0.33rem;
 `;
 
 const Navbar = () => (
-  <nav className="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
+  <NavbarWrapper className="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item is-info">
@@ -35,21 +40,21 @@ const Navbar = () => (
           </Link>
           <div className="navbar-item"></div>
           <SocialLink className="navbar-item" href="https://github.com/bradmcgonigle" rel="noopener noreferrer">
-            <span className="icon"><FontAwesomeIcon icon={faGithub} inverse /></span>
+            <span className="icon"><FontAwesomeIcon icon={faGithub} /></span>
           </SocialLink>
           <SocialLink className="navbar-item" href="https://instagram.com/bradmcgonigle" rel="noopener noreferrer">
-            <span className="icon"><FontAwesomeIcon icon={faInstagram} inverse /></span>
+            <span className="icon"><FontAwesomeIcon icon={faInstagram} /></span>
           </SocialLink>
           <SocialLink className="navbar-item" href="https://twitter.com/bradmcgonigle" rel="noopener noreferrer">
-            <span className="icon"><FontAwesomeIcon icon={faTwitter} inverse /></span>
+            <span className="icon"><FontAwesomeIcon icon={faTwitter} /></span>
           </SocialLink>
           <SocialLink className="navbar-item" href="https://facebook.com/bradmcgonigle" rel="noopener noreferrer">
-            <span className="icon"><FontAwesomeIcon icon={faFacebook} inverse /></span>
+            <span className="icon"><FontAwesomeIcon icon={faFacebook} /></span>
           </SocialLink>
         </div>
       </div>
     </div>
-  </nav>
+  </NavbarWrapper>
 );
 
 export default Navbar;
