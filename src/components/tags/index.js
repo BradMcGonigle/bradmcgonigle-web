c4b94b60901e0295f1720182afcb2792d5dd0d3e
Brad McGonigle <brad@bradmcgonigle.com>
2018-02-09 15:24:26 -0500

    Add link detail page - Adds `tags` component for post tagging

---
+++ src/components/tags/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'react-emotion';
+
+const TagList = styled('div')`
+  border-top: 2px solid #f9f9f9;
+  padding-top: 1.5rem;
+  margin-top: 1.5rem;
+`;
+
+export default function Tags({ list = [], ignore }) {
+  const tags = list.filter((a) => {return a !== ignore});
+  return (
+    <TagList className="tags">
+      {tags.map(tag =>
+        <span className="tag is-size-6 has-text-weight-light is-lowercase">{tag}</span>
+      )}
+    </TagList>
+  );
+}
