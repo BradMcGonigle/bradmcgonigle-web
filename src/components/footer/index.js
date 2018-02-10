59a3618d379b28866b6da2e3de328f3534f5f9f4
Brad McGonigle <brad@bradmcgonigle.com>
2018-02-09 19:15:09 -0500

    Center footer text on mobile

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,10 +5,10 @@ const Footer = () => (
   <footer className="footer">
     <div className="container is-fluid">
       <div className="columns content">
-        <div className="column is-6">
+        <div className="column is-6 has-text-centered-mobile">
           <p className="is-size-7 has-text-weight-light">&copy; {(new Date().getFullYear())}. All rights reserved.</p>
         </div>
-        <div className="column is-6 has-text-right">
+        <div className="column is-6 has-text-right-tablet has-text-centered-mobile">
           <p className="is-size-7 has-text-weight-light">
             Built with <a href="http://www.gatsbyjs.org">Gatsby</a> and <a href="http://bulma.io">Bulma</a> running on <a href="https://netlify.com">Netlify</a>.
           </p>
