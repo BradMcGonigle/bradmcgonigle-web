60c8544304a161d7b3ff3acb2db1e3925ce9dfc5
Brad McGonigle <brad@bradmcgonigle.com>
2019-04-22 15:14:20 -0400

    Fix mobile navigation

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -21,35 +21,54 @@ const SocialLink = styled(Navbar.Item)`
   }
 `;
 
-const Nav = () => (
-  <NavbarWrapper className="is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
-    <Navbar.Brand>
-      <Link to="/" className="navbar-item is-info">
-        <strong>B &mdash; M.</strong>
-      </Link>
-      <Navbar.Burger />
-    </Navbar.Brand>
-    <Navbar.Menu>
-      <Navbar.Container position="end" className="has-text-centered-mobile">
-        <Link to="/about" className="navbar-item site-nav">About</Link>
-        <Link to="/work" className="navbar-item site-nav">Work</Link>
-        <Link to="/links" className="navbar-item site-nav">Links</Link>
-        <Navbar.Item className="is-hidden-mobile"></Navbar.Item>
-        <SocialLink href="https://github.com/bradmcgonigle" rel="noopener noreferrer">
-          <span className="icon"><FontAwesomeIcon icon={faGithub} /></span>
-        </SocialLink>
-        <SocialLink href="https://instagram.com/bradmcgonigle" rel="noopener noreferrer">
-          <span className="icon"><FontAwesomeIcon icon={faInstagram} /></span>
-        </SocialLink>
-        <SocialLink href="https://twitter.com/bradmcgonigle" rel="noopener noreferrer">
-          <span className="icon"><FontAwesomeIcon icon={faTwitter} /></span>
-        </SocialLink>
-        <SocialLink href="https://facebook.com/bradmcgonigle" rel="noopener noreferrer">
-          <span className="icon"><FontAwesomeIcon icon={faFacebook} /></span>
-        </SocialLink>
-      </Navbar.Container>
-    </Navbar.Menu>
-  </NavbarWrapper>
-);
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default Nav;
+    this.state = {
+       open: false,
+     }
+  }
+
+  onClick = () => {
+    console.log(this.state.open);
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+  render() {
+    return (
+      <NavbarWrapper className="is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
+        <Navbar.Brand>
+          <Link to="/" className="navbar-item is-info">
+            <strong>B &mdash; M.</strong>
+          </Link>
+          <Navbar.Burger active={this.state.open ? 1 : 0} onClick={this.onClick} />
+        </Navbar.Brand>
+        <Navbar.Menu active={this.state.open ? 1 : 0}>
+          <Navbar.Container position="end" className="has-text-centered-mobile">
+            <Link to="/about" className="navbar-item site-nav">About</Link>
+            <Link to="/work" className="navbar-item site-nav">Work</Link>
+            <Link to="/links" className="navbar-item site-nav">Links</Link>
+            <Navbar.Item className="is-hidden-mobile"></Navbar.Item>
+            <SocialLink href="https://github.com/bradmcgonigle" rel="noopener noreferrer">
+              <span className="icon"><FontAwesomeIcon icon={faGithub} /></span>
+            </SocialLink>
+            <SocialLink href="https://instagram.com/bradmcgonigle" rel="noopener noreferrer">
+              <span className="icon"><FontAwesomeIcon icon={faInstagram} /></span>
+            </SocialLink>
+            <SocialLink href="https://twitter.com/bradmcgonigle" rel="noopener noreferrer">
+              <span className="icon"><FontAwesomeIcon icon={faTwitter} /></span>
+            </SocialLink>
+            <SocialLink href="https://facebook.com/bradmcgonigle" rel="noopener noreferrer">
+              <span className="icon"><FontAwesomeIcon icon={faFacebook} /></span>
+            </SocialLink>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </NavbarWrapper>
+    )
+  }
+}
+
+export default Nav
