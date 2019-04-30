28e80b78248b47f3f15e03f4b9d762f44cec05a0
Brad McGonigle <brad@bradmcgonigle.com>
2019-04-30 16:49:26 -0400

    Hide work section for now

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,35 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/fontawesome-pro-light';
 
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <section className="section">
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-6">
-            <h1 className="title has-text-weight-light">
-              404 &mdash; Page Not Found
-            </h1>
-            <h4 className="has-text-weight-semibold margin-bottom-half">
-              Well, this is embarrassing!
-            </h4>
-            <p>
-              We couldn't find the page you are looking for. Most likely it
-              doesn't exist.
-            </p>
+    <div className={`hero is-fullheight is-bold is-primary`}>
+      <div className="hero-body">
+        <div className="container content">
+          <div className="columns">
+            <div className="column is-6">
+              <h1 className="title has-text-weight-light">
+                404 &mdash; Page Not Found
+              </h1>
+              <h2 className="subtitle has-text-light has-text-weight-semibold">
+                Well, this is embarrassing!
+              </h2>
+              <p>
+                We couldn't find the page you are looking for.
+              </p>
+              <p>
+                <a href="/" className="button is-light is-outlined">
+                  <span className="icon"><FontAwesomeIcon icon={faArrowLeft} /></span> &nbsp; Go Home
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   </Layout>
 )
 
