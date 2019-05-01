ab8e9d1e53cd95f5a2f2969808f8f98224622662
Brad McGonigle <brad@bradmcgonigle.com>
2019-05-01 14:23:53 -0400

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/fontawesome-pro-light';
+import { Columns, Container, Content, Heading, Hero } from 'react-bulma-components'
 
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div className={`hero is-fullheight is-bold is-primary`}>
-      <div className="hero-body">
-        <div className="container content">
-          <div className="columns">
-            <div className="column is-6">
-              <h1 className="title has-text-weight-light">
-                404 &mdash; Page Not Found
-              </h1>
-              <h2 className="subtitle has-text-light has-text-weight-semibold">
-                Well, this is embarrassing!
-              </h2>
-              <p>
-                We couldn't find the page you are looking for.
-              </p>
-              <p>
-                <a href="/" className="button is-light is-outlined">
-                  <span className="icon"><FontAwesomeIcon icon={faArrowLeft} /></span> &nbsp; Go Home
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Hero className="is-fullheight is-bold is-primary">
+      <Hero.Body>
+        <Container>
+          <Content>
+            <Columns>
+              <Columns.Column size={6}>
+                <Heading size={1} className="has-text-weight-light">
+                  404 &mdash; Page Not Found
+                </Heading>
+                <Heading subtitle size={3} className="has-text-light has-text-weight-semibold">
+                  Well, this is embarrassing!
+                </Heading>
+                <p>
+                  We couldn't find the page you are looking for.
+                </p>
+                <p>
+                  <a href="/" className="button is-light is-outlined">
+                    <span className="icon"><FontAwesomeIcon icon={faArrowLeft} /></span> &nbsp; Go Home
+                  </a>
+                </p>
+              </Columns.Column>
+            </Columns>
+          </Content>
+        </Container>
+      </Hero.Body>
+    </Hero>
   </Layout>
 )
 
