3f7beb6cb685587417be65d38e96a34c7073c364
Brad McGonigle <brad@bradmcgonigle.com>
2019-08-06 22:01:21 -0400

    Set list key properly for blog index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,15 +15,15 @@ export const BlogIndex = ({ data }) => {
 
   return (
     <Layout>
-      <SEO keywords={[`blog`]} title="Blog" />
+      <SEO keywords={[`blog`]} title="Blog" url="/about" />
       <Section>
         <Container>
           <SectionHeader section="Writings" tagline="Thoughts on things" />
           <Columns>
             {posts.map(({ node: post }, i, { length }) => (
-              <React.Fragment>
+              <React.Fragment key={post.id}>
                 {i === 0 && (
-                  <Columns.Column size={12} key={post.id}>
+                  <Columns.Column size={12}>
                     <BlogFeaturedPostTease post={post} />
                   </Columns.Column>
                 )}
@@ -32,7 +32,7 @@ export const BlogIndex = ({ data }) => {
           </Columns>
           <Columns>
             {posts.map(({ node: post }, i, { length }) => (
-              <React.Fragment>
+              <React.Fragment key={post.id}>
                 {i !== 0 && i <= 3 && (
                   <Columns.Column size={4} key={post.id}>
                     <BlogPostTeaseCard post={post} />
@@ -43,9 +43,9 @@ export const BlogIndex = ({ data }) => {
           </Columns>
           <Columns>
             {posts.map(({ node: post }, i, { length }) => (
-              <React.Fragment>
+              <React.Fragment key={post.id}>
                 {i > 3 && (
-                  <Columns.Column size={6} key={post.id}>
+                  <Columns.Column size={6}>
                     <BlogPostTease post={post} />
                   </Columns.Column>
                 )}
